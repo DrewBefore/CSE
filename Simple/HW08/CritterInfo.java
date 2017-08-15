7097ec5fb6f5fdad1f1cb823676dbdd891ca5320
drewbefore <dbefore@uw.edu>
2017-08-15 12:56:37 -0700

    removed simple

--- Simple/HW08/CritterInfo.java
+++
@@ -1,8 +0,0 @@
-public interface CritterInfo {
-    public Critter.Neighbor getFront();
-    public Critter.Neighbor getBack();
-    public Critter.Neighbor getLeft();
-    public Critter.Neighbor getRight();
-    public Critter.Direction getDirection();
-    public int getInfectCount();
-}
